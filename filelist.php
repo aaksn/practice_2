1ded94e5997a5d67496587fd3d4c1fd86d7dc282
Anton Aksenov <34284157+aaksn@users.noreply.github.com>
2019-07-01 00:58:50 +0300

    Раюочее отображение

--- filelist.php
+++ filelist.php
@@ -1,7 +1,19 @@
 <?php
 //Отображение списка таблиц
 if (!empty($_GET["id"])) {
-	echo " Data id:".$_GET["id"]."" ;
+	$id = $_GET["id"];
+	//echo " Data id:".$_GET["id"]."" ;
+	$courses = [
+		[1 => "1 группа", 2 => "2 группа", 3 => "5 группа"],
+		[1 => "2 группа", 2 => "1 группа", 3 => "4 группа"],
+		[1 => "1 группа", 2 => "2 группа"],
+		[1 => "1 группа", 2 => "2 группа", 3 => "3 группа"],
+		[1 => "1 группа", 2 => "2 группа", 3 => "4 группа"],
+		[1 => "1 группа", 2 => "2 группа", 3 => "3 группа"]
+	];
+	foreach ($courses[$id] as $key => $value) {
+		echo '<li id='.$key.'f"><input type="radio" id="'.$key.'" name="groupselector" /><label for="'.$key.'">'.$value.'</label><a href="javascript:editpos('.$key.');" class="button primary small icon fa-pencil-square-o">Изменить</a> <a href="javascript:deletepos('.$key.');" class="button primary small icon fa-trash">Удалить</a></li>';
+	}
 }
 
 //Редактирование списка таблиц
