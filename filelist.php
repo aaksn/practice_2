1326fe6a5056be15fa03c190c25fe879e6903ba7
Anton Aksenov <34284157+aaksn@users.noreply.github.com>
2019-07-02 19:47:46 +0300

    Обновление списка предметов

--- filelist.php
+++ filelist.php
@@ -10,7 +10,7 @@ if (!empty($_GET["course"])) {
     //echo " Data id:".$_GET["id"]."" ;
     $link = mysqli_connect($host, $user, $password, $database)
     or die("Ошибка " . mysqli_error($link));
-    $query = "SELECT * FROM SUBJECTS WHERE ID_COURSE=$id";
+    $query = "SELECT * FROM subjects WHERE ID_COURSE=$id";
     $result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link));
     if ($result) {
         $subjects = []; // Массив для хранения данных
@@ -38,7 +38,7 @@ if (!empty($_GET["id"])) {
     $link = mysqli_connect($host, $user, $password, $database)
     or die("Ошибка " . mysqli_error($link));
     // выполняем операции с базой данных
-    $query = "SELECT * FROM GROUPS WHERE ID_COURSE=$id";
+    $query = "SELECT * FROM groups WHERE ID_COURSE=$id";
     $result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link));
     if ($result) {
         $course = []; // Массив для хранения данных
@@ -68,9 +68,9 @@ if (!empty($_POST["idtable"]) && !empty($_POST["type"])) {
         $link = mysqli_connect($host, $user, $password, $database)
         or die("Ошибка " . mysqli_error($link));
         // выполняем операции с базой данных
-        $n = mysqli_query($link, "SELECT max(ID_GROUP) as maxid FROM GROUPS WHERE ID_COURSE=$id") or die("Ошибка " . mysqli_error($link));
+        $n = mysqli_query($link, "SELECT max(ID_GROUP) as maxid FROM groups WHERE ID_COURSE=$id") or die("Ошибка " . mysqli_error($link));
         $u = mysqli_fetch_row($n)[0] + 1;
-        mysqli_query($link, "INSERT INTO GROUPS (`ID_GROUP`, `GROUP_NAME`, `ID_COURSE`)  VALUES ($u ,'$u группа',$id)") or die("Ошибка " . mysqli_error($link));
+        mysqli_query($link, "INSERT INTO groups (`ID_GROUP`, `GROUP_NAME`, `ID_COURSE`)  VALUES ($u ,'$u группа',$id)") or die("Ошибка " . mysqli_error($link));
         // закрываем подключение
         mysqli_close($link);
 
@@ -84,7 +84,7 @@ if (!empty($_POST["idtable"]) && !empty($_POST["type"])) {
         $link = mysqli_connect($host, $user, $password, $database)
         or die("Ошибка " . mysqli_error($link));
         // выполняем операции с базой данных
-        mysqli_query($link, "DELETE FROM GROUPS WHERE ID_GROUP=$groupid AND ID_COURSE=$id") or die("Ошибка " . mysqli_error($link));
+        mysqli_query($link, "DELETE FROM groups WHERE ID_GROUP=$groupid AND ID_COURSE=$id") or die("Ошибка " . mysqli_error($link));
         // закрываем подключение
         mysqli_close($link);
 
@@ -98,7 +98,7 @@ if (!empty($_POST["idtable"]) && !empty($_POST["type"])) {
         $link = mysqli_connect($host, $user, $password, $database)
         or die("Ошибка " . mysqli_error($link));
         // выполняем операции с базой данных
-        mysqli_query($link, "UPDATE GROUPS SET GROUP_NAME='$data' WHERE ID_GROUP=$groupid AND ID_COURSE=$id") or die("Ошибка " . mysqli_error($link));
+        mysqli_query($link, "UPDATE groups SET GROUP_NAME='$data' WHERE ID_GROUP=$groupid AND ID_COURSE=$id") or die("Ошибка " . mysqli_error($link));
         // закрываем подключение
         mysqli_close($link);
 
