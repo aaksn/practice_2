0f693d2b28c882e742864a54ad968a4174d3d5a3
Ruslan <34807817+pieceee@users.noreply.github.com>
2019-07-02 15:07:24 +0300

    sql+update
    Добавил пиздатую бд и исправил вывод\удаление групп

---
+++ db.sql
@@ -0,0 +1,300 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Июл 02 2019 г., 15:04
+-- Версия сервера: 10.3.13-MariaDB
+-- Версия PHP: 7.1.22
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `attendance`
+--
+
+CREATE TABLE `attendance` (
+  `ID_ATT` int(11) NOT NULL,
+  `ID_SUBJECT` int(11) NOT NULL,
+  `ID_STUDENT` int(11) NOT NULL,
+  `DATE_POS` date NOT NULL,
+  `MARK` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `attendance`
+--
+
+INSERT INTO `attendance` (`ID_ATT`, `ID_SUBJECT`, `ID_STUDENT`, `DATE_POS`, `MARK`) VALUES
+(1, 2, 1, '2019-07-01', 1),
+(2, 2, 4, '2019-07-01', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `courses`
+--
+
+CREATE TABLE `courses` (
+  `ID_COURSE` int(11) NOT NULL,
+  `COURSE_NAME` varchar(30) NOT NULL,
+  `ID_GROUP` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `courses`
+--
+
+INSERT INTO `courses` (`ID_COURSE`, `COURSE_NAME`, `ID_GROUP`) VALUES
+(1, '1 курс', NULL),
+(2, '2 курс', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `groups`
+--
+
+CREATE TABLE `groups` (
+  `ID_GROUP` int(11) NOT NULL,
+  `GROUP_NAME` varchar(30) NOT NULL,
+  `ID_COURSE` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `groups`
+--
+
+INSERT INTO `groups` (`ID_GROUP`, `GROUP_NAME`, `ID_COURSE`) VALUES
+(1, '1 группа', 1),
+(1, '1 группа', 2),
+(2, '2 группа', 1),
+(2, '2 группа', 2),
+(3, '3 группа', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `permissions`
+--
+
+CREATE TABLE `permissions` (
+  `ID_PERMISSION` int(11) NOT NULL,
+  `RANK` int(11) NOT NULL,
+  `NAME_RANK` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `permissions`
+--
+
+INSERT INTO `permissions` (`ID_PERMISSION`, `RANK`, `NAME_RANK`) VALUES
+(1, 777, 'root'),
+(2, 111, 'user'),
+(3, 222, 'teacher'),
+(4, 666, 'moderator');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `students`
+--
+
+CREATE TABLE `students` (
+  `ID_STUDENT` int(11) NOT NULL,
+  `FIO` varchar(70) NOT NULL,
+  `ID_GROUP` int(11) NOT NULL,
+  `ID_COURSE` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `students`
+--
+
+INSERT INTO `students` (`ID_STUDENT`, `FIO`, `ID_GROUP`, `ID_COURSE`) VALUES
+(1, 'Иванов А.А.', 1, 1),
+(2, 'Петров У.М', 1, 1),
+(3, 'Плякин У.К.', 2, 1),
+(4, 'Кобз П.К.', 2, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `subjects`
+--
+
+CREATE TABLE `subjects` (
+  `ID_SUBJECT` int(11) NOT NULL,
+  `SUBJECT_NAME` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `subjects`
+--
+
+INSERT INTO `subjects` (`ID_SUBJECT`, `SUBJECT_NAME`) VALUES
+(2, 'ТИПИС'),
+(1, 'УрМат');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `ID_USER` tinyint(2) UNSIGNED NOT NULL,
+  `USERNAME` varchar(15) NOT NULL,
+  `PASSWORD` varchar(15) NOT NULL,
+  `RANK` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`ID_USER`, `USERNAME`, `PASSWORD`, `RANK`) VALUES
+(2, 'root', 'root', 777),
+(3, 'teacher', 'teacher', 222),
+(4, 'moderator', 'moderator', 666),
+(5, 'user', 'user', 111);
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `attendance`
+--
+ALTER TABLE `attendance`
+  ADD PRIMARY KEY (`ID_ATT`),
+  ADD KEY `ID_SUBJECT` (`ID_SUBJECT`),
+  ADD KEY `ID_STUDENT` (`ID_STUDENT`);
+
+--
+-- Индексы таблицы `courses`
+--
+ALTER TABLE `courses`
+  ADD PRIMARY KEY (`ID_COURSE`);
+
+--
+-- Индексы таблицы `groups`
+--
+ALTER TABLE `groups`
+  ADD PRIMARY KEY (`ID_GROUP`,`ID_COURSE`),
+  ADD KEY `GROUPS_ibfk_1` (`ID_COURSE`);
+
+--
+-- Индексы таблицы `permissions`
+--
+ALTER TABLE `permissions`
+  ADD PRIMARY KEY (`ID_PERMISSION`),
+  ADD UNIQUE KEY `RANK` (`RANK`),
+  ADD KEY `RANK_2` (`RANK`);
+
+--
+-- Индексы таблицы `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`ID_STUDENT`),
+  ADD KEY `ID_COURSE` (`ID_COURSE`),
+  ADD KEY `ID_GROUP` (`ID_GROUP`);
+
+--
+-- Индексы таблицы `subjects`
+--
+ALTER TABLE `subjects`
+  ADD PRIMARY KEY (`ID_SUBJECT`),
+  ADD UNIQUE KEY `subject_name` (`SUBJECT_NAME`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`ID_USER`),
+  ADD UNIQUE KEY `username` (`USERNAME`),
+  ADD KEY `RANK` (`RANK`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `attendance`
+--
+ALTER TABLE `attendance`
+  MODIFY `ID_ATT` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT для таблицы `permissions`
+--
+ALTER TABLE `permissions`
+  MODIFY `ID_PERMISSION` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT для таблицы `students`
+--
+ALTER TABLE `students`
+  MODIFY `ID_STUDENT` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT для таблицы `subjects`
+--
+ALTER TABLE `subjects`
+  MODIFY `ID_SUBJECT` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `ID_USER` tinyint(2) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `attendance`
+--
+ALTER TABLE `attendance`
+  ADD CONSTRAINT `ATTENDANCE_ibfk_2` FOREIGN KEY (`ID_SUBJECT`) REFERENCES `subjects` (`ID_SUBJECT`),
+  ADD CONSTRAINT `ATTENDANCE_ibfk_3` FOREIGN KEY (`ID_STUDENT`) REFERENCES `students` (`ID_STUDENT`);
+
+--
+-- Ограничения внешнего ключа таблицы `groups`
+--
+ALTER TABLE `groups`
+  ADD CONSTRAINT `GROUPS_ibfk_1` FOREIGN KEY (`ID_COURSE`) REFERENCES `courses` (`ID_COURSE`);
+
+--
+-- Ограничения внешнего ключа таблицы `students`
+--
+ALTER TABLE `students`
+  ADD CONSTRAINT `STUDENTS_ibfk_1` FOREIGN KEY (`ID_COURSE`) REFERENCES `courses` (`ID_COURSE`),
+  ADD CONSTRAINT `STUDENTS_ibfk_2` FOREIGN KEY (`ID_GROUP`) REFERENCES `groups` (`ID_GROUP`);
+
+--
+-- Ограничения внешнего ключа таблицы `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `USERS_ibfk_1` FOREIGN KEY (`RANK`) REFERENCES `permissions` (`RANK`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- filelist.php
+++ filelist.php
@@ -1,108 +1,105 @@
 <?php
 $host = 'localhost';
-$database = 'courses'; // имя базы данных
+$database = 'db'; // имя базы данных
 $user = 'root'; // имя пользователя
 $password = ''; // пароль
 //Отображение списка таблиц
 if (!empty($_GET["id"])) {
-	//$id = $_GET["id"] - 1;
-	//echo " Data id:".$_GET["id"]."" ;
-	$id = $_GET["id"];
-	// подключаемся к серверу
-	$link = mysqli_connect($host, $user, $password, $database) 
-	    or die("Ошибка " . mysqli_error($link));
+    //$id = $_GET["id"] - 1;
+    //echo " Data id:".$_GET["id"]."" ;
+    $id = $_GET["id"];
+    // подключаемся к серверу
+    $link = mysqli_connect($host, $user, $password, $database)
+    or die("Ошибка " . mysqli_error($link));
     // выполняем операции с базой данных
-    $coursetables = "c".$_GET["id"];
-    $query ="SELECT * FROM ".$coursetables;
-	$result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link));
-	if($result)
-	{
-		$course = []; // Массив для хранения данных
-	    $rows = mysqli_num_rows($result); // количество полученных строк
-	    
-	    for ($i = 0 ; $i < $rows ; ++$i)
-	    {
-	        $row = mysqli_fetch_row($result);
-	        $course[$row[0]] = $row[1];
-	    };
-	    
-	    //Выводим результат
-		foreach ($course as $key => $value) {
-			echo '<li id='.$key.'f"><input type="radio" id="'.$key.'" name="groupselector" /><label for="'.$key.'">'.$value.'</label><a href="javascript:editpos('.$key.');" class="button primary small icon fa-pencil-square-o">Изменить</a> <a href="javascript:deletepos('.$key.');" class="button primary small icon fa-trash">Удалить</a></li>';
-	     
-	    // очищаем результат
-	    //mysqli_free_result($result);
-	    };  
-    	// закрываем подключение
-    	mysqli_close($link);
-	
+    $coursetables = "GROUPS";//.$_GET["id"];
+    $query ="SELECT * FROM $coursetables WHERE ID_COURSE=$id";
+    $result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link));
+    if($result)
+    {
+        $course = []; // Массив для хранения данных
+        $rows = mysqli_num_rows($result); // количество полученных строк
 
-    	//Пример
-    	/*$courses = [
-    		[1 => "1 группа", 2 => "2 группа", 3 => "5 группа"],
-    		[1 => "2 группа", 2 => "1 группа", 3 => "4 группа"],
-    		[1 => "1 группа", 2 => "2 группа"],
-    		[1 => "1 группа", 2 => "2 группа", 3 => "3 группа"],
-    		[1 => "1 группа", 2 => "2 группа", 3 => "4 группа"],
-    		[1 => "1 группа", 2 => "2 группа", 3 => "3 группа"]
-    	];
-    	foreach ($courses[$id] as $key => $value) {
-    		echo '<li id='.$key.'f"><input type="radio" id="'.$key.'" name="groupselector" /><label for="'.$key.'">'.$value.'</label><a href="javascript:editpos('.$key.');" class="button primary small icon fa-pencil-square-o">Изменить</a> <a href="javascript:deletepos('.$key.');" class="button primary small icon fa-trash">Удалить</a></li>';
-    	};*/
+        for ($i = 0 ; $i < $rows ; ++$i)
+        {
+            $row = mysqli_fetch_row($result);
+            $course[$row[0]] = $row[1];
         };
+
+        //Выводим результат
+        foreach ($course as $key => $value) {
+            echo '<li id='.$key.'f"><input type="radio" id="'.$key.'" name="groupselector" /><label for="'.$key.'">'.$value.'</label><a href="javascript:editpos('.$key.');" class="button primary small icon fa-pencil-square-o">Изменить</a> <a href="javascript:deletepos('.$key.');" class="button primary small icon fa-trash">Удалить</a></li>';
+
+            // очищаем результат
+            //mysqli_free_result($result);
+        };
+        // закрываем подключение
+        mysqli_close($link);
+
+
+        //Пример
+        /*$courses = [
+            [1 => "1 группа", 2 => "2 группа", 3 => "5 группа"],
+            [1 => "2 группа", 2 => "1 группа", 3 => "4 группа"],
+            [1 => "1 группа", 2 => "2 группа"],
+            [1 => "1 группа", 2 => "2 группа", 3 => "3 группа"],
+            [1 => "1 группа", 2 => "2 группа", 3 => "4 группа"],
+            [1 => "1 группа", 2 => "2 группа", 3 => "3 группа"]
+        ];
+        foreach ($courses[$id] as $key => $value) {
+            echo '<li id='.$key.'f"><input type="radio" id="'.$key.'" name="groupselector" /><label for="'.$key.'">'.$value.'</label><a href="javascript:editpos('.$key.');" class="button primary small icon fa-pencil-square-o">Изменить</a> <a href="javascript:deletepos('.$key.');" class="button primary small icon fa-trash">Удалить</a></li>';
+        };*/
+    };
 };
 
 //Редактирование списка таблиц
 if (!empty($_POST["idtable"])&&!empty($_POST["type"])) {
-	if ($_POST["type"] == "ADD") {
-	    $id = $_POST["idtable"];
-		// подключаемся к серверу
-		$link = mysqli_connect($host, $user, $password, $database) 
-		    or die("Ошибка " . mysqli_error($link));
-	    // выполняем операции с базой данных
-	    $coursetables = "c".$id;
-	    $query ="INSERT INTO `".$coursetables."` (`groupid`, `name`) VALUES (NULL, '0 группа')";
-		$result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link));
-		$n = mysqli_query($link, "SELECT LAST_INSERT_ID() FROM ".$coursetables) or die("Ошибка " . mysqli_error($link));
-		$u = mysqli_fetch_row($n)[0];
-		mysqli_query($link, "UPDATE `$coursetables` SET `name` = '$u группа' WHERE `$coursetables`.`groupid` = $u") or die("Ошибка " . mysqli_error($link));
-		// закрываем подключение
-		mysqli_close($link);
-		 
-		echo "$u";
-	}
-	if ($_POST["type"] == 'DEL' && !empty($_POST["id"])) {
-		$data = $_POST["data"];
-		$id = $_POST["idtable"];
-		$groupid = $_POST["id"];
-		// подключаемся к серверу
-		$link = mysqli_connect($host, $user, $password, $database) 
-		    or die("Ошибка " . mysqli_error($link));
-	    // выполняем операции с базой данных
-	    $coursetables = "c".$id;
-	    
-		mysqli_query($link, "DELETE FROM `$coursetables` WHERE `$coursetables`.`groupid` = $groupid") or die("Ошибка " . mysqli_error($link));
-		// закрываем подключение
-		mysqli_close($link);
-		 
-		echo "Deleted";
-	}
-	if ($_POST["type"] == 'EDIT' && !empty($_POST["data"]) && !empty($_POST["id"])) {
-		$data = $_POST["data"];
-		$id = $_POST["idtable"];
-		$groupid = $_POST["id"];
-		// подключаемся к серверу
-		$link = mysqli_connect($host, $user, $password, $database) 
-		    or die("Ошибка " . mysqli_error($link));
-	    // выполняем операции с базой данных
-	    $coursetables = "c".$id;
-	    
-		mysqli_query($link, "UPDATE `$coursetables` SET `name` = '$data' WHERE `$coursetables`.`groupid` = $groupid") or die("Ошибка " . mysqli_error($link));
-		// закрываем подключение
-		mysqli_close($link);
-		 
-		echo "Edited";
-	}
+    if ($_POST["type"] == "ADD") {
+        $id = $_POST["idtable"];
+        // подключаемся к серверу
+        $link = mysqli_connect($host, $user, $password, $database)
+        or die("Ошибка " . mysqli_error($link));
+        // выполняем операции с базой данных
+        $coursetables = "GROUPS";
+        $n = mysqli_query($link, "SELECT max(ID_GROUP) as maxid FROM $coursetables WHERE ID_COURSE=$id") or die("Ошибка " . mysqli_error($link));
+        $u=mysqli_fetch_row($n)[0]+1;
+        mysqli_query($link, "INSERT INTO $coursetables (`ID_GROUP`, `GROUP_NAME`, `ID_COURSE`)  VALUES ($u ,'$u группа',$id)") or die("Ошибка " . mysqli_error($link));
+        // закрываем подключение
+        mysqli_close($link);
+
+        echo "$u";
+    }
+    if ($_POST["type"] == 'DEL' && !empty($_POST["id"])) {
+        $data = $_POST["data"];
+        $id = $_POST["idtable"];
+        $groupid = $_POST["id"];
+        // подключаемся к серверу
+        $link = mysqli_connect($host, $user, $password, $database)
+        or die("Ошибка " . mysqli_error($link));
+        // выполняем операции с базой данных
+        $coursetables = "GROUPS";
+        mysqli_query($link, "DELETE FROM $coursetables WHERE ID_GROUP=$groupid AND ID_COURSE=$id") or die("Ошибка " . mysqli_error($link));
+        // закрываем подключение
+        mysqli_close($link);
+
+        echo "Deleted";
+    }
+    if ($_POST["type"] == 'EDIT' && !empty($_POST["data"]) && !empty($_POST["id"])) {
+        $data = $_POST["data"];
+        $id = $_POST["idtable"];
+        $groupid = $_POST["id"];
+        // подключаемся к серверу
+        $link = mysqli_connect($host, $user, $password, $database)
+        or die("Ошибка " . mysqli_error($link));
+        // выполняем операции с базой данных
+        $coursetables = "c".$id;
+
+        mysqli_query($link, "UPDATE $coursetables SET GROUP_NAME = $data WHERE ID_GROUP = $groupid") or die("Ошибка " . mysqli_error($link));
+        // закрываем подключение
+        mysqli_close($link);
+
+        echo "Edited";
+    }
 }
 
 
