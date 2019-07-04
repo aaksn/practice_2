9f19076fd31b1865dde68648798e99dc665f38cc
Ruslan <34807817+pieceee@users.noreply.github.com>
2019-07-04 20:43:31 +0300

    написал запросы

--- table.php
+++ table.php
@@ -1,28 +1,38 @@
 <?php
+$host = 'localhost';
+$database = 'db'; // имя базы данных
+$user = 'root'; // имя пользователя
+$password = ''; // пароль
+
 //Отображение таблицы
-if (!empty($_GET["idtable"])) {
-    // idtable - course
-    if (!empty($_GET["id"])) {
-        //echo " Data id:".$_GET["id"]."" ;
+if (!empty($_POST["courseid"]) && !empty($_POST["subjectid"]) && !empty($_POST["groupid"])) {
+    $courseid = $_POST["courseid"];
+    $groupid = $_POST["groupid"];
+    $subjectid = $_POST["subjectid"];
+}
+// idtable - course
+if (!empty($_GET["id"])) {
+    //echo " Data id:".$_GET["id"]."" ;
 
-        $names = [1 => "Аксенов А.А.", 2 => "Погорелов Р. И.", 3 => "Сычев А.В."];
-        $dates = [1 => "1.01", 2 => "2.02", 3 => "3.03", 4 => "4.04", 5 => "5.05"];
-        $marks = [1 => [TRUE, TRUE, FALSE, TRUE, FALSE], 2 => [TRUE, TRUE, FALSE, TRUE, FALSE], 3 => [FALSE, TRUE, FALSE, FALSE, FALSE]];
+    $link = mysqli_connect($host, $user, $password, $database)
+    or die("Ошибка " . mysqli_error($link));
+    //запрос успеваемости
+    $query = "SELECT students.FIO,attendance.DATE_POS,attendance.MARK FROM attendance,students WHERE students.ID_STUDENT=attendance.ID_STUDENT AND attendance.ID_SUBJECT=$subjectid AND students.ID_GROUP=$groupid AND students.ID_COURSE=$courseid";
+    $result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link));
+    if ($result) {
+        $subjects = []; // Массив для хранения данных
+        $rows = mysqli_num_rows($result); // количество полученных строк
 
-        $t1 = [$names, $dates, $marks];
-        $t2 = [$names, $dates, $marks];
-        $t3 = [$names, $dates, $marks];
+        for ($i = 0; $i < $rows; ++$i) {
+            $row = mysqli_fetch_row($result);
+            $subjects[$row[0]] = $row[1];
+        };
 
-        $c1 = [$t1, $t2, $t3];
-        $c2 = [$t1, $t2, $t3];
-        $c3 = [$t1, $t3];
-        $c4 = [$t1, $t2, $t3];
-        $c5 = [$t1, $t2, $t3];
-        $c6 = [$t1, $t2, $t3];
+        //Пример
+        foreach ($subjects as $key => $value) {
+            echo '<option value="' . $key . '">' . $value . '</option>';
+        };
 
-        $c = [$c1, $c2, $c3, $c4, $c5, $c6];
-
-        $t = $c[$_GET["idtable"] - 1][$_GET["id"] - 1];
         echo "" . count($t[2]) . "";
         echo "<thead><tr><th>Имя</th>";
         foreach ($t[1] as $key => $value) {
@@ -51,14 +61,76 @@ if (!empty($_GET["idtable"])) {
 }
 
 //Редактирование таблицы
-if (!empty($_POST["type"]) && !empty($_POST["idtable"])) {
-    if ($_POST["type"] == 'ADDSTUDENT') {
+if (!empty($_POST["type"]) && !empty($_POST["courseid"]) && !empty($_POST["subjectid"]) && !empty($_POST["groupid"])) {
+    $courseid = $_POST["courseid"];
+    $groupid = $_POST["groupid"];
+    $subjectid = $_POST["subjectid"];
+
+    if ($_POST["type"] == 'ADDSTUDENT' && !empty($_POST["fio"])) {
+        $fio = $_POST["fio"];
+
+        $link = mysqli_connect($host, $user, $password, $database) or die("Ошибка " . mysqli_error($link));
+        // выполняем операции с базой данных
+        //добавляем студента
+        mysqli_query($link, "INSERT INTO students(`FIO`, `ID_GROUP`, `ID_COURSE`) VALUES ($fio,$groupid,$courseid)") or die("Ошибка " . mysqli_error($link));
+        //ищем его id
+        $qid = mysqli_query($link, "SELECT ID_STUDENT FROM students WHERE FIO=$fio AND ID_COURSE=$courseid AND ID_GROUP=$groupid") or die("Ошибка " . mysqli_error($link));
+        $arr_studentid = mysqli_fetch_row($qid);
+        //ищем все даты его группы на этои предмете
+        $dates = mysqli_query($link, "SELECT DATE_POS as datee FROM attendance WHERE ID_STUDENT=(SELECT ID_STUDENT FROM attendance WHERE ID_GROUP=$groupid AND ID_COURSE=$courseid AND ID_SUBJECT=$subjectid) LIMIT 1") or die("Ошибка " . mysqli_error($link));
+        $arr_dates = mysqli_fetch_row($dates);
+        //добавляем все даты в успеваемость
+        for ($i = 0; $i < mysqli_num_rows($dates); $i++) {
+            mysqli_query($link, "INSERT INTO attendance (`ID_SUBJECT`, `ID_STUDENT`, `DATE_POS`) VALUES ($subjectid,$arr_studentid[0],$arr_dates[$i])") or die("Ошибка " . mysqli_error($link));
+        }
+        // закрываем подключение
+        mysqli_close($link);
+
         echo "Type: ADDSTUDENT Table id is:" . $_POST["idtable"] . "";
     }
-    if ($_POST["type"] == 'CHANGENAME' && !empty($_POST["id"]) && !empty($_POST["data"])) {
+    if ($_POST["type"] == 'ADDDATE' && !empty($_POST["fio"])) {
+        $date = $_POST["date"];
+        $link = mysqli_connect($host, $user, $password, $database) or die("Ошибка " . mysqli_error($link));
+        // выполняем операции с базой данных
+        //ищем всех сдуентов с данными группой, курсом, предметом
+        $students = mysqli_query($link, "SELECT students.ID_STUDENT FROM attendance,students WHERE students.ID_STUDENT=attendance.ID_STUDENT AND attendance.ID_SUBJECT=$subjectid AND students.ID_GROUP=$groupid AND students.ID_COURSE=$courseid") or die("Ошибка " . mysqli_error($link));
+        $arr_student = mysqli_fetch_row($dates);
+        //добавляем дату в успеваемость для всех студентов
+        for ($i = 0; $i < mysqli_num_rows($students); $i++) {
+            mysqli_query($link, "INSERT INTO attendance (`ID_SUBJECT`, `ID_STUDENT`, `DATE_POS`) VALUES ($subjectid,$arr_student[$i],'$date')") or die("Ошибка " . mysqli_error($link));
+        }
+        // закрываем подключение
+        mysqli_close($link);
+
+        echo "Type: ADDSTUDENT Table id is:" . $_POST["idtable"] . "";
+    }
+    if ($_POST["type"] == 'CHANGENAME' && !empty($_POST["fio"])) {
+        $fio_old = $_GET["fio"];//старые фио
+        $fio = $_POST["fio"];//новые фио
+        $link = mysqli_connect($host, $user, $password, $database)
+        or die("Ошибка " . mysqli_error($link));
+        // выполняем операции с базой данных
+        mysqli_query($link, "UPDATE students SET FIO=$fio WHERE FIO=$fio_old AND ID_COURSE=$courseid AND ID_GROUP=$groupid") or die("Ошибка " . mysqli_error($link));
+        // закрываем подключение
+        mysqli_close($link);
+
         echo "Type: CHANGENAME Id is:" . $_POST["idtable"] . "";
     }
     if ($_POST["type"] == 'CHANGEDATE' && !empty($_POST["id"]) && !empty($_POST["data"])) {
+        $date_old=$_GET["date"];//старая дата
+        $date = $_POST["date"];
+        $link = mysqli_connect($host, $user, $password, $database) or die("Ошибка " . mysqli_error($link));
+        // выполняем операции с базой данных
+        //ищем всех сдуентов с данными группой, курсом, предметом
+        $students = mysqli_query($link, "SELECT students.ID_STUDENT FROM attendance,students WHERE students.ID_STUDENT=attendance.ID_STUDENT AND attendance.ID_SUBJECT=$subjectid AND students.ID_GROUP=$groupid AND students.ID_COURSE=$courseid") or die("Ошибка " . mysqli_error($link));
+        $arr_student = mysqli_fetch_row($dates);
+        //изменяем дату в успеваемости для всех студентов
+        for ($i = 0; $i < mysqli_num_rows($students); $i++) {
+            mysqli_query($link, "UPDATE attendance SET DATE_POS=$date WHERE DATE_POS=$date_old AND ID_STUDENT=$arr_student[$i] AND ID_COURSE=$courseid AND ID_GROUP=$groupid AND ID_SUBJECT=$subjectid") or die("Ошибка " . mysqli_error($link));
+        }
+        // закрываем подключение
+        mysqli_close($link);
+
         echo "Type: CHANGEDATE Id is:" . $_POST["idtable"] . "";
     }
 }
