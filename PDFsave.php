6a8a39d417c85908a56b1f4f183e57ae22b8737b
Ruslan <34807817+pieceee@users.noreply.github.com>
2019-09-02 14:58:36 +0300

    savePDF

--- PDFsave.php
+++ PDFsave.php
@@ -1,25 +1,55 @@
 <?php
 require_once("dbcontroller.php");
-$db_handle = new DBController();
-$result = $db_handle->runQuery("SELECT * FROM toy");
-$header = $db_handle->runQuery("SELECT `COLUMN_NAME` 
-FROM `INFORMATION_SCHEMA`.`COLUMNS` 
-WHERE `TABLE_SCHEMA`='blog_samples' 
-    AND `TABLE_NAME`='toy'");
+$host = 'localhost';
+$database = 'db'; // имя базы данных
+$user = 'root'; // имя пользователя
+$password = ''; // пароль
+$link = mysqli_connect($host, $user, $password, $database) or die("Ошибка " . mysqli_error($link));
 
-require('fpdf/fpdf.php');
-$pdf = new FPDF();
-$pdf->AddPage();
-$pdf->SetFont('Arial','B',12);
-foreach($header as $heading) {
-    foreach($heading as $column_heading)
-        $pdf->Cell(90,12,$column_heading,1);
+//$db_handle = new DBController();
+if (!empty($_GET["courseid"]) && !empty($_GET["subjectid"]) && !empty($_GET["groupid"])) {
+    $courseid = $_GET["courseid"];
+    $groupid = $_GET["groupid"];
+    $subjectid = $_GET["subjectid"];
+    $outputpath=$_GET["output"];
+    $students = [];
+    $dates = [];
+    $res = mysqli_query($link, "SELECT ID_STUDENT,FIO FROM students WHERE students.ID_COURSE=$courseid AND students.ID_GROUP=$groupid");
+    while ($row = mysqli_fetch_row($res)) {
+        $mark = [];
+        $marks = mysqli_query($link, "SELECT attendance.ID_ATT, attendance.MARK FROM attendance,students WHERE attendance.ID_STUDENT=$row[0] AND students.ID_STUDENT=attendance.ID_STUDENT AND attendance.ID_SUBJECT=$subjectid AND students.ID_GROUP=$groupid AND students.ID_COURSE=$courseid");
+        while ($row2 = mysqli_fetch_row($marks)) {
+            $mark[$row2[0]] = $row2[1];
+        }
+        $students[] = array("name" => $row[1], "marks" => $mark);
+    }
+    $resdate = mysqli_query($link, "SELECT DISTINCT dates.DATE FROM students,attendance,dates WHERE students.ID_STUDENT=attendance.ID_STUDENT AND attendance.ID_DATE=dates.ID_DATE AND students.ID_COURSE=$courseid AND students.ID_GROUP=$groupid AND attendance.ID_SUBJECT=$subjectid");
+    while ($row = mysqli_fetch_row($resdate)) {
+        $dates[] = $row[0];
+    }
+//define('FPDF_FONTPATH','fpdf/font');
+    require('fpdf/tfpdf.php');
+    $pdf = new tFPDF();
+    $pdf->AddFont('DejaVu', '', 'DejaVuSansCondensed.ttf', true);
+    $pdf->AddPage();
+    $pdf->SetFont('DejaVu', '', 10);
+    $width = 52;
+    $pdf->Cell($width, 10);
+    foreach ($dates as $date) {
+
+        $pdf->Cell(16, 10, $date, 1);
+    }
+    foreach ($students as $student) {
+        $pdf->SetFont('DejaVu', '', 10);
+        $pdf->Ln();
+        $pdf->Cell($width, 10, $student["name"], 1);
+        foreach ($student["marks"] as $mark)
+            if ($mark == '1') {
+                $pdf->Cell(16, 10, '+', 1, 0, 'C');
+            } else {
+                $pdf->Cell(16, 10, '-', 1, 0, 'C');
+            }
+    }
+    $pdf->Output($outputpath);
 }
-foreach($result as $row) {
-    $pdf->SetFont('Arial','',12);
-    $pdf->Ln();
-    foreach($row as $column)
-        $pdf->Cell(90,12,$column,1);
-}
-$pdf->Output();
 ?>
