3b89e60187d9b58f99cdd1d5f37e20994159252e
Ruslan <34807817+pieceee@users.noreply.github.com>
2019-09-02 09:36:39 +0300

    сократил

--- table.php
+++ table.php
@@ -12,43 +12,24 @@ if (!empty($_GET["courseid"]) && !empty($_GET["subjectid"]) && !empty($_GET["gro
 
     $link = mysqli_connect($host, $user, $password, $database)
     or die("Ошибка " . mysqli_error($link));
-    //запрос успеваемости
-    /*
-    $datequery = "SELECT DISTINCT dates.ID_DATE, dates.DATE FROM attendance,students,dates WHERE students.ID_STUDENT=attendance.ID_STUDENT AND attendance.ID_DATE=dates.ID_DATE AND attendance.ID_SUBJECT=$subjectid AND students.ID_GROUP=$groupid AND students.ID_COURSE=$courseid";
-    $studentsquery = "SELECT DISTINCT students.ID_STUDENT, students.FIO FROM attendance,students WHERE students.ID_STUDENT=attendance.ID_STUDENT AND attendance.ID_SUBJECT=$subjectid AND students.ID_GROUP=$groupid AND students.ID_COURSE=$courseid";
-    
-    $dateresult = mysqli_query($link, $datequery) or die("Ошибка " . mysqli_error($link));
-    $studentsresult = mysqli_query($link, $studentsquery) or die("Ошибка " . mysqli_error($link));
-
-    $dates = []; // Массив для хранения дат
-    $rows = mysqli_num_rows($dateresult); // количество полученных строк
-
-    for ($i = 0; $i < $rows; ++$i) {
-        $row = mysqli_fetch_row($dateresult);
-        $dates[$row[0]] = $row[1];
-    };
-
-    $students = []; // Массив для хранения студентов
-    $rows2 = mysqli_num_rows($studentsresult); // количество полученных строк
-
-    for ($i = 0; $i < $rows2; ++$i) {
-        $row2 = mysqli_fetch_row($studentsresult);
-        $marksquery = "SELECT attendance.ID_ATT, attendance.MARK FROM attendance,students WHERE attendance.ID_STUDENT=$row2[0] AND students.ID_STUDENT=attendance.ID_STUDENT AND attendance.ID_SUBJECT=$subjectid AND students.ID_GROUP=$groupid AND students.ID_COURSE=$courseid";
-        $marksresult = mysqli_query($link, $marksquery) or die("Ошибка " . mysqli_error($link));
-        $rows3 = mysqli_num_rows($marksresult); // количество полученных строк
-        $marks = [];
-        for ($j = 0; $j < $rows3; ++$j) {
-            $row3 = mysqli_fetch_row($marksresult);
-            $marks[$row3[0]] = $row3[1];            
-        };        
-        $students[] = array("id" => $row2[0], "name" => $row2[1], "marks" => $marks);
-    };
-    */
-    $stud=mysqli_fetch_row(mysqli_query($link, "SELECT students.ID_STUDENT,students.FIO,attendance.ID_ATT, attendance.ID_DATE, dates.DATE, attendance.MARK FROM students,attendance,dates WHERE students.ID_STUDENT=attendance.ID_STUDENT AND attendance.ID_DATE=dates.ID_DATE AND students.ID_COURSE=$courseid AND students.ID_GROUP=$groupid AND attendance.ID_SUBJECT=$subjectid") or die("Ошибка " . mysqli_error($link)));
-    $students[] = array("id" => $stud[0], "name" => $stud[1],"iddate"=>$stud[3],"date"=>$stud[4], "marks" => $stud[5]);
+    $students=[];
+    $dates=[];
+    $res=mysqli_query($link, "SELECT students.ID_STUDENT,students.FIO FROM students WHERE students.ID_COURSE=$courseid AND students.ID_GROUP=$groupid") or die("Ошибка " . mysqli_error($link));
+     while($row = mysqli_fetch_row($res)){
+         $mark=[];
+         $marks = mysqli_query($link, "SELECT attendance.ID_ATT, attendance.MARK FROM attendance,students WHERE attendance.ID_STUDENT=$row[0] AND students.ID_STUDENT=attendance.ID_STUDENT AND attendance.ID_SUBJECT=$subjectid AND students.ID_GROUP=$groupid AND students.ID_COURSE=$courseid") or die("Ошибка " . mysqli_error($link));
+         while ($row2 = mysqli_fetch_row($marks)){
+             $mark[$row2[0]] = $row2[1];  }
+        $students[] = array("id" => $row[0], "name" => $row[1], "marks" => $mark);
+    }
+    $resdate=mysqli_query($link, "SELECT DISTINCT attendance.ID_DATE, dates.DATE FROM students,attendance,dates WHERE students.ID_STUDENT=attendance.ID_STUDENT AND attendance.ID_DATE=dates.ID_DATE AND students.ID_COURSE=$courseid AND students.ID_GROUP=$groupid AND attendance.ID_SUBJECT=$subjectid") or die("Ошибка " . mysqli_error($link));
+    while($row = mysqli_fetch_row($resdate)){
+        $iddate=$row[0];
+        $dates[$iddate] = $row[1];
+    }
     echo json_encode(array(
         "students" => $students,
-        //"dates" => $dates
+        "dates" => $dates
     ));
     exit;    
 }
