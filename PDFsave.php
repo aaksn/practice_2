0c26b2fd39c4a3d6da78c64f0ef4c7d6e92f9387
aaksn <34284157+aaksn@users.noreply.github.com>
2019-09-03 18:49:15 +0300

    Update PDFsave.php

--- PDFsave.php
+++ PDFsave.php
@@ -6,6 +6,9 @@ $password = ''; // пароль
 $link = mysqli_connect($host, $user, $password, $database) or die("Ошибка " . mysqli_error($link));
 
 if (!empty($_GET["courseid"]) && !empty($_GET["subjectid"]) && !empty($_GET["groupid"])) {
+    if ($_GET["groupid"] == 'undefined' | $_GET["subjectid"] == 'undefined') {
+        exit;
+    }
     header('Content-Type: application/pdf');
     $courseid = $_GET["courseid"];
     $groupid = $_GET["groupid"];
